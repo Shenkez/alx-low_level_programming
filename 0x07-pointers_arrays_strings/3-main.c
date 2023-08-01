4c91631e6b2701aacd52729600aa387fc75b9402
Terry <shenkez23@gmail.com>
2023-08-01 09:22:17 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/3-main.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
+}
+julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strspn.c -o 3-strspn
+julien@ubuntu:~/0x07$ ./3-strspn 
+5
+julien@ubuntu:~/0x07$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x07-pointers_arrays_strings
+File: 3-strspn.c
+   
+4. strpbrk
+mandatory
+Write a function that searches a string for any of a set of bytes.
+
+Prototype: char *_strpbrk(char *s, char *accept);
+The _strpbrk() function locates the first occurrence in the string s of any of the bytes in the string accept
+Returns a pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
+FYI: The standard library provides a similar function: strpbrk. Run man strpbrk to learn more.
+
+julien@ubuntu:~/0x07$ cat 4-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
+    return (0);
+}

d5b9020129674c6d02417e51612959aff3301ab4
shenkez <shenkez23@gmail.com>
2023-08-10 04:42:52 -0700

    memory allocation

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-
 /**
  * malloc_checked - check the code
  * @b : memory size
@@ -13,15 +11,12 @@ void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(sizeof(b));
+	ptr = malloc(b);
 
 
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	else
-	{
-		return (ptr);
-	}
+	return (ptr);
 }

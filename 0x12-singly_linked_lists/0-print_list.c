0636a1918190305cb63bc05db9a42e15723bc949
Terry (shenkez) <shenkez23@gmail.com>
2023-09-22 10:27:10 -0700

    elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,15 +15,14 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
+			printf("[0](nil)\n");
 		}
 		else
 		{
 			printf("%u %s\n", h->len, h->str);
 		}
-
-			h = h->next;
-			count++;
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
